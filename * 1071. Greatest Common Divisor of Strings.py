f3cd521346e6c101991d946291ed94ffdd1b7259
allenwxf <dog0120@163.com>
2024-07-04 13:46:12 -0400

    Add some reflections

--- * 1071. Greatest Common Divisor of Strings.py
+++ * 1071. Greatest Common Divisor of Strings.py
@@ -1,40 +1,15 @@
-"""
-For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t
-(i.e., t is concatenated with itself one or more times).
-
-Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
-
-
-
-Example 1:
-
-Input: str1 = "ABCABC", str2 = "ABC"
-Output: "ABC"
-
-Example 2:
-
-Input: str1 = "ABABAB", str2 = "ABAB"
-Output: "AB"
-
-Example 3:
-
-Input: str1 = "LEET", str2 = "CODE"
-Output: ""
-
-
-Constraints:
-
-1 <= str1.length, str2.length <= 1000
-str1 and str2 consist of English uppercase letters.
-"""
-
 import math
 
 
 class Solution1:
     """
+    2024-07-04 Some Reflection:
+    Key point: use iteration to check whether they have common base
+    TIME: O(n)
+
     use prefix string
     """
+
     @staticmethod
     def gcdOfStrings(str1: str, str2: str) -> str:
         len1, len2 = len(str1), len(str2)
@@ -55,8 +30,14 @@ print(Solution1().gcdOfStrings("ABABABAB", "ABAB"))
 
 class Solution2:
     """
+    2024-07-04 Reflection:
+    Key assumption: AB = BA, suppose len(A) >= len(B), then A = Bxxx...B → AB = Bxxx...BB → A = Bxxx...BB → ...
+    Conslusion: A and B must share a common base!
+    problem turns into: n * base = m * base, here base is the greatest common divisor
+
     gcd solution
     """
+
     @staticmethod
     def gcdOfStrings(str1: str, str2: str) -> str:
         if str1 + str2 != str2 + str1:
