038a4f671e545784bf755d1631cfd83934a94c18
allenwxf <dog0120@163.com>
2024-07-29 16:02:58 -0400

    Reviews - use double-pointer will enhance the readability of the code

--- 345. Reverse Vowels of a String.py
+++ 345. Reverse Vowels of a String.py
@@ -1,3 +1,26 @@
+class Solution2:
+    """
+    Use double-pointer
+    """
+
+    @staticmethod
+    def reverseVowels(s: str) -> str:
+        s = list(s)
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        start, end = 0, len(s) - 1
+
+        while start < end:
+            if s[start].lower() not in vowels:
+                start += 1
+            elif s[end].lower() not in vowels:
+                end -= 1
+            else:
+                # s[start], s[end] = s[end], s[start]  # Error: 'str' object does not support item assignment
+                s[start], s[end] = s[end], s[start]
+                start += 1
+                end -= 1
+        return "".join(s)
+
 
 class Solution1:
     @staticmethod
@@ -13,11 +36,14 @@ class Solution1:
                     if i >= j:
                         return "".join(res_list)
 
-                    if res_list[j-1].lower() in vowels:
-                        tmp_val = res_list[j-1]
-                        res_list[j-1] = res_list[i]
+                    if res_list[j - 1].lower() in vowels:
+                        tmp_val = res_list[j - 1]
+                        res_list[j - 1] = res_list[i]
                         res_list[i] = tmp_val
-                        last_pos = j-1
+                        last_pos = j - 1
                         break
 
         return "".join(res_list)
+
+
+
