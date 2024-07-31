d4d28aff8539f8dcc88ace184e5df7d6bbec072a
allenwxf <dog0120@163.com>
2024-07-31 15:24:35 -0400

    Reviews - two pointers to ensure constant extra space

---
+++ ! 443. String Compression.py
@@ -0,0 +1,68 @@
+from typing import List
+
+"""
+Better solution: use two pointers: one to store the current position to write, one to store the read point
+"""
+
+
+class Solution2:
+    """
+    double pointer solution
+    """
+
+    @staticmethod
+    def compress(chars: List[str]) -> int:
+        walker, runner = 0, 0
+        while runner < len(chars):
+
+            chars[walker] = chars[runner]
+            count = 1
+
+            while runner + 1 < len(chars) and chars[runner] == chars[runner + 1]:
+                runner += 1
+                count += 1
+
+            if count > 1:
+                for c in str(count):
+                    chars[walker + 1] = c
+                    walker += 1
+
+            runner += 1
+            walker += 1
+
+        return walker
+
+
+print(Solution2.compress(["a", "b", "c"]))
+
+
+class Solution:
+    @staticmethod
+    def compress(chars: List[str]) -> int:
+        cnt, chars_pos, charslen = 1, 0, len(chars)
+
+        if charslen == 1:
+            return charslen
+
+        for i in range(1, charslen):
+            if chars[i] == chars[i - 1]:
+                cnt += 1
+
+            a, b = chars[i], chars[i - 1]  # !
+            if a != b or i == charslen - 1:
+                chars[chars_pos] = b
+                chars_pos += 1
+                if cnt > 1:
+                    for num_str in str(cnt):
+                        chars[chars_pos] = num_str
+                        chars_pos += 1
+                cnt = 1
+
+                if chars_pos <= i == charslen - 1 and a != b:
+                    chars[chars_pos] = a
+                    chars_pos += 1
+
+        return chars_pos
+
+
+print(Solution.compress(["a", "b", "c"]))
