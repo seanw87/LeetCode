e6e75fb9d719a14b518c7f278bccb7a5653c693f
allenwxf <dog0120@163.com>
2024-07-31 15:05:47 -0400

    Reviews - multiplicative from left or right

--- ! 238. Product of Array Except Self.py
+++ ! 238. Product of Array Except Self.py
@@ -1,6 +1,11 @@
 from typing import List
 
-
+"""
+e.g. [1, 2, 3, 4]
+pre_val: [1, 1, 2, 6, 24]  # accumulative multiply from left to right
+post_val: [1, 4, 12, 24, 24]  # accumulative multiply from right to left
+res = [pre[3] * post[0], pre[2] * post[1], pre[1] * post[2], pre[0] * post[3]]
+"""
 class Solution:
     @staticmethod
     def productExceptSelf(nums: List[int]) -> List[int]:
