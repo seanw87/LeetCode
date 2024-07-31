638bef047976835d30d3a0f436e77e14bce6fbb2
allenwxf <dog0120@163.com>
2024-07-31 15:16:52 -0400

    Reviews - value cascade comparison to find increasing triplet or consective-n subsequence

--- ! 334. Increasing Triplet Subsequence.py
+++ ! 334. Increasing Triplet Subsequence.py
@@ -1,7 +1,12 @@
 from typing import List
 import math
 
-
+"""
+solution: value pass
+if right value is smaller than left value, then assign a to the smaller value
+if right value is not smaller than a and smaller than b, then assign b to the smaller value
+else (right value is bigger than b, and b is bigger than a) return true 
+"""
 class Solution:
     @staticmethod
     def increasingTriplet(nums: List[int]) -> bool:
