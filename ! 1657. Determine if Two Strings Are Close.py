9132730e9087308025df05de45265b8b33a85c53
allenwxf <dog0120@163.com>
2024-08-21 16:23:03 -0400

    Review: 1. collections.Counter().keys() and collections.Counter().values() 2. := assignment of variables within expressions (>= Python 3.8)

--- ! 1657. Determine if Two Strings Are Close.py
+++ ! 1657. Determine if Two Strings Are Close.py
@@ -1,6 +1,11 @@
 import collections
 
-
+"""
+Find the characteristics: The value of different letters and the position of either letter can be exchanged,
+which means the deduplicated letters and their overall distribution should be the same.
+Use collections.Counter().keys() to get the deduplicated letters
+Use collections.Counter().values() to get the distribution of these letters 
+"""
 class Solution:
     """
     if the distribution of the counts of value keeps the same, it's "close" to the original word
