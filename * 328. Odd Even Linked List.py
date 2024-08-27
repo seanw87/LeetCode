079a02a63be35f64c6c896a9f718165d8bee911c
allenwxf <dog0120@163.com>
2024-08-27 10:56:57 -0400

    Review: difference between:     (1) ll = head.next     (2) ll.next = ll.next.next The first won't change the Linked List itself, ll just got the reference of the Linked List The second will change the Linked List, since the reference of the next node of the Linked List has been redirected to other nodes(next next node in this case)

--- * 328. Odd Even Linked List.py
+++ * 328. Odd Even Linked List.py
@@ -1,6 +1,15 @@
 # Definition for singly-linked list.
 from typing import Optional
 
+"""
+difference between:
+    (1) ll = head.next 
+    (2) ll.next = ll.next.next
+The first won't change the Linked List itself, ll just got the reference of the Linked List
+The second will change the Linked List, since the reference of the next node of the Linked List has been 
+redirected to other nodes(next next node in this case)
+
+"""
 
 class ListNode:
     def __init__(self, val=0, next=None):
