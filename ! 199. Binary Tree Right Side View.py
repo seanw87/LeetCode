09b765baabb005ea30829188a217656cd5a87fe1
allenwxf <dog0120@163.com>
2024-09-14 11:14:26 -0400

    Review: Solution 1: typical BFS Solution 2: typical DFS Solution 3: customized DFS (projection to the right side and supplement the left side(if right side not existed)

--- ! 199. Binary Tree Right Side View.py
+++ ! 199. Binary Tree Right Side View.py
@@ -1,7 +1,11 @@
 from typing import Optional, List
 from math import sqrt, floor, pow
 
-
+"""
+Solution 1: typical BFS
+Solution 2: typical DFS
+Solution 3: customized DFS (projection to the right side and supplement the left side(if right side not existed)
+"""
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -10,10 +14,78 @@ class TreeNode:
         self.right = right
 
 
+class Solution3:
+    """
+    BFS solution: iterate each layer
+    """
+
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        if root:
+            level = [root]
+            while level:
+                res.append(level[-1].val)
+                level = [kid for node in level for kid in (node.left, node.right) if kid]
+
+        return res
+
+
+class Solution:
+    """
+    DFS solution: since each layer return the right most node,
+    so the size of the result can be regarded as the total layers.
+    """
+
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        res = []
+
+        def dfs(node, curDepth):
+            if not node:
+                return
+
+            if curDepth == len(res):
+                res.append(node.val)
+
+            dfs(node.right, curDepth + 1)
+            dfs(node.left, curDepth + 1)
+
+        dfs(root, 0)
+        return res
+
+
+class Solution2:
+    """
+    combine right and left:
+    for the branch of each layer, get the right most node, if not existed, get the left node(left[len(right):])
+    [root.val]: [4] right: [] left: []
+    [root.val]: [5] right: [] left: [4]
+    [root.val]: [2] right: [] left: []
+    [root.val]: [3] right: [5, 4] left: [2]
+    [root.val]: [1] right: [3, 5, 4] left: []
+    [root.val]: [6] right: [] left: [1, 3, 5, 4]
+
+    TC: O(n^2) (list concatenation in return is O(n^2))
+    """
+
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        right = self.rightSideView(root.right)
+        left = self.rightSideView(root.left)
+
+        print("[root.val]:", [root.val], "right:", right, "left:", left)
+
+        return [root.val] + right + left[len(right):]
+
+
 class WrongSolution:
     """
     try to store the layer # in a hash by numbering the nodes, but nodes may not exist.
     """
+
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
@@ -41,70 +113,6 @@ class WrongSolution:
         return res
 
 
-class Solution:
-    """
-    DFS solution: since each layer return the right most node,
-    so the size of the result can be regarded as the total layers.
-    """
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-
-        res = []
-
-        def dfs(node, curDepth):
-            if not node:
-                return
-
-            if curDepth == len(res):
-                res.append(node.val)
-
-            dfs(node.right, curDepth+1)
-            dfs(node.left, curDepth+1)
-
-        dfs(root, 0)
-        return res
-
-
-class Solution2:
-    """
-    combine right and left:
-    for the branch of each layer, get the right most node, if not existed, get the left node(left[len(right):])
-    [root.val]: [4] right: [] left: []
-    [root.val]: [5] right: [] left: [4]
-    [root.val]: [2] right: [] left: []
-    [root.val]: [3] right: [5, 4] left: [2]
-    [root.val]: [1] right: [3, 5, 4] left: []
-    [root.val]: [6] right: [] left: [1, 3, 5, 4]
-
-    TC: O(n^2) (list concatenation in return is O(n^2))
-    """
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-        right = self.rightSideView(root.right)
-        left = self.rightSideView(root.left)
-
-        print("[root.val]:", [root.val], "right:", right, "left:", left)
-
-        return [root.val] + right + left[len(right):]
-
-
-class Solution3:
-    """
-    BFS solution: iterate each layer
-    """
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
-        if root:
-            level = [root]
-            while level:
-                res.append(level[-1].val)
-                level = [kid for node in level for kid in (node.left, node.right) if kid]
-
-        return res
-
-
 tree = TreeNode(val=6)
 tree.left = TreeNode(val=1)
 tree.left.right = TreeNode(val=3)
@@ -116,4 +124,4 @@ tree.left.right.left.right = TreeNode(val=101)
 tree.left.right.right = TreeNode(val=5)
 tree.left.right.right.left = TreeNode(val=4)
 
-print(Solution3().rightSideView(tree))
+print(Solution2().rightSideView(tree))
